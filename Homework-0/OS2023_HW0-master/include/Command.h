66ec4cd2f605fc8e4ea132805862a58d0584885f
Konstantinos Arkoulis <100994360+ConstArc@users.noreply.github.com>
2024-07-03 17:50:01 +0300

    Delete Homework-0/OS2023_HW0-master directory

--- Homework-0/OS2023_HW0-master/include/Command.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Global.h"
-
-// The function that initializes the database and runs it, for as long as the program runs
-void RunDB(char* file_path, int m, int bucket_size, float LOAD_THRESHOLD);
